36325b6e25a93bc034506d950a2b8ad6289f8a81
Behruz Erkinov <erkinovbehruz0@gmail.com>
2024-12-08 03:52:07 +0500

    first attempt to fix mobile responsiveness

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -6,19 +6,15 @@ function Banner() {
     <div className="banner-container">
       <div className="banner-content">
         <h1 className="banner-title">
-          SIZNING BIZNESINGIZNI<br />
-          RIVOJLANTIRISH UCHUN<br />
-          MUKAMMAL <span className="highlight-it">'IT'</span> YECHIMLAR!
+          SIZNING BIZNESINGIZNI RIVOJLANTIRISH UCHUN MUKAMMAL <span className="highlight-it">'IT'</span> YECHIMLAR!
         </h1>
         <p className="banner-description">
-          Loyihangizni 10 yildan ortiq tajribaga ega<br />
-          mutaxassislarga ishonib topshiring va qisqa vaqt ichida<br />
-          yuqori muvaffaqiyatlarga erishing.
+          Loyihangizni 10 yildan ortiq tajribaga ega mutaxassislarga ishonib topshiring va qisqa vaqt ichida yuqori muvaffaqiyatlarga erishing.
         </p>
         <button className="contact-button">Aloqa</button>
       </div>
       <div className="banner-image-container">
-        <img src={bannerImg} alt="Banner" className="banner-image" />
+        <img src={bannerImg} alt="Banner" className="banner-image" loading="lazy" />
       </div>
     </div>
   );
